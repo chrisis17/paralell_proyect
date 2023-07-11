130103e6514e7cfb1e576b10754ab899f4927bb6
Paul Rios <paul.rios@utec.edu.pe>
2023-07-10 23:59:27 -0500

    si

--- omptsp.cpp
+++ omptsp.cpp
@@ -5,7 +5,7 @@
 #include <omp.h>
 
 using namespace std;
-int num_threads = 1;
+int num_threads = 2;
 vector<vector<int>> crearMatrizIncidencia(const string& archivo) {
     ifstream entrada(archivo);
     if (!entrada) {
